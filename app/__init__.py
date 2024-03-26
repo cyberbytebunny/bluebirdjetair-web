e27d7aa506e5126eedd70df14558b727b880cde9
Emily Ernest <cyberbytebunny@gmail.com>
2024-03-25 23:15:37 -0700

    editted some files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 import os
+from os.path import join, dirname
 
 def create_app():
-    config = dotenv_values('.env')
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
     
-    secret_key = config.get('SECRET_KEY', 'secret').encode()
-    admin_id = config.get('ADMIN_ID', 'admin123')
-    flag = config.get('FLAG', 'THIS_IS_NOT_THE_FLAG')
+    secret_key = os.environ.get('SECRET_KEY', 'secret').encode()
+    admin_id = os.environ.get('ADMIN_ID', 'admin123')
+    flag = os.environ.get('FLAG', 'THIS_IS_NOT_THE_FLAG')
 
     app = Flask(__name__)
     app.secret_key = secret_key
